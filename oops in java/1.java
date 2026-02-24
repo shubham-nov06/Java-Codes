14dc1cefba11931282a475240bbe01e1063a16b2
shubham-nov06 <dograshubham06@gmail.com>
2026-02-24 14:38:30 +0530

    Added file 1.java

---
+++ oops in java/1.java
@@ -0,0 +1,20 @@
+class constructor {
+    int num ;
+    int age ;
+ 
+ constructor() {
+    num = 90 ;
+    age = 20 ;
+ }   
+
+constructor( int i , int j ) {
+    i = 20 ;
+    j = 30;
+    System.err.println(i,j);
+} 
+
+
+
+
+
+}
