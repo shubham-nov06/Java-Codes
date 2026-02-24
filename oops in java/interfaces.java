bccc28118479c7d83dc610b574e7c6a7c0d69e2c
shubham-nov06 <dograshubham06@gmail.com>
2026-02-24 14:38:31 +0530

    Added file interfaces.java

--- oops in java/interfaces.java
+++ oops in java/interfaces.java
@@ -1,3 +1,20 @@
-public class interfaces {
-    
+class constructor {
+    int num ;
+    int age ;
+ 
+ constructor() {
+    num = 90 ;
+    age = 20 ;
+ }   
+
+constructor( int i , int j ) {
+    i = 20 ;
+    j = 30;
+    System.err.println(i,j);
+} 
+
+
+
+
+
 }
