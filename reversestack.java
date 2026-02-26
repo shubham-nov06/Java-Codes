84a6b174c0c90e257c8ede2f1a5cecf171ea4bc1
shubham-nov06 <dograshubham06@gmail.com>
2026-02-26 06:07:58 +0000

    Updated changes

---
+++ reversestack.java
@@ -0,0 +1,18 @@
+importjava.util>Stack
+
+public class reversestack {
+    
+    public static void main(String[] args ) {
+        Stack<Integer> s = new Stack<>();
+        rev.push(1);
+        rev.push(2);
+        rev.push(3);
+
+        Stack<Integer> rev = new Stack<>();
+        while(!s.isEmpty()) {
+            rev.push(s.pop());
+
+        }
+        system.out.println("Reversed stack is " + rev);
+    }
+}
