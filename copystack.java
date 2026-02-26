b24d5cc173ed88af4d6afcf43ae7f1d562de6bb9
shubham-nov06 <dograshubham06@gmail.com>
2026-02-26 06:17:06 +0000

    Updated changes

---
+++ copystack.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class copystack {
+    public static void main(Stiring[] args) {
+
+        Stack<Integer> n = new Stack<>();
+        n.push(100);
+        n.push(200);
+
+    }
+}
