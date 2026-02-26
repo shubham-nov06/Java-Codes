772c38924a5f5da21f12961f6d1961800bb17716
shubham-nov06 <dograshubham06@gmail.com>
2026-02-26 06:07:58 +0000

    Updated changes

--- reversestack.java
+++ reversestack.java
@@ -4,9 +4,9 @@ public class reversestack {
     
     public static void main(String[] args ) {
         Stack<Integer> s = new Stack<>();
-        rev.push(1);
-        rev.push(2);
-        rev.push(3);
+        s.push(1);
+        s.push(2);
+        s.push(3);
 
         Stack<Integer> rev = new Stack<>();
         while(!s.isEmpty()) {
